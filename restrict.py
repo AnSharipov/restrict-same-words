787804a5e78d90076cc38f706784726ba9e56c82
AnSharipov <andreyartdirector@yandex.ru>
2017-01-30 01:43:50 +0700

    Update restrict.py

--- restrict.py
+++ restrict.py
@@ -1,7 +1,8 @@
 #How to delete the same emails from one file
 from collections import OrderedDict
+import sys
 
-with open('emails.txt') as fin: #here we add our file, it must be .txt and all emails must be in one row
+with open(sys.argv[1]) as fin: #here we add our file adress, it must be .txt and all emails must be in one row
     lines = (line.rstrip() for line in fin)
     unique_lines = OrderedDict.fromkeys( (line for line in lines if line) )
 
